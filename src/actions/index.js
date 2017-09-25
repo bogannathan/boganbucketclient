0f5b076335f13808f658258109f42581709a6b30
Nathan Bogan <bogannathan@gmail.com>
2017-09-25 09:14:34 -0400

    changed index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -37,7 +37,6 @@ export function signinUser({ email, password }){
 		axios.post(`${ROOT_URL}/signin`, {email, password})
 				.then(response => {
 					dispatch({ type: AUTH_USER });
-
 					localStorage.setItem('token', response.data.token);
 					browserHistory.push('/newitem');
 				})
